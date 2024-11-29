42e72561c3c269a7dfb1c57f41a2c36d308b6240
PAH911 <phamanhhuy9111@gmail.com>
2024-11-30 01:39:39 +0700

    add_JS_1

---
+++ index.js
@@ -0,0 +1,20 @@
+// Add event chuyển tab giữa các bài
+let Bai1 = document.querySelector("#bai1");
+let Bai2 = document.querySelector("#bai2");
+
+// Bai1.addEventListener("click", function () {
+//   document.querySelector(".content").innerText = "";
+// });
+
+// Bài 1
+
+// Input
+let luong1ngay = 20;
+let songay = 28;
+
+// Xử lý
+let luongtong = 0;
+luongtong = luong1ngay * songay;
+
+// Output
+console.log("Tổng lương =", luongtong);
